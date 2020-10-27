19bfc90ffc9c4c04eee5c3a2fccc7b64a50dc786
lpm57 <lpm57@alumnos.unican.es>
2020-10-27 20:14:52 +0100

    Continuacion

--- CUDA/add_two_matrix.cu
+++ CUDA/add_two_matrix.cu
@@ -1,14 +1,27 @@
 #include <stdio.h>
-#define N 1000
-// 1 thread: 1.56 segundos
+#include <sys/time.h>
+
+#ifndef N 
+	#define N 3500
+#endif
+
+#ifndef numBlock
+	#define numBlock 1
+#endif
+
+#ifndef numThreadsPerBlock
+	#define numThreadsPerBlock 2
+#endif	
+
 __global__ void addMatrix(int* a, int* b, int* result, int block)
 {
 	int idx = blockDim.x*blockIdx.x + threadIdx.x;
-	printf("Hola mundo desde el device con thread: %d\n",idx);
+	//printf("Hola mundo desde el device con thread: %d\n",idx);
 	for (int i = block*idx; i <block*(idx + 1); i++)
 			result[i] = a[i] + b[i];
 }
 
+// print matrix indicated by argument
 void 
 printMtx(int * matrix)
 {
@@ -16,16 +29,30 @@ printMtx(int * matrix)
 	for (i = 0; i < N; i++)
     {
         for (j = 0; j < N; j++)
-        {
             printf("%d\t", matrix[i*N+j]);
-        }
         printf("\n");
     }
+}
+
+double time() {
+	struct timeval time;
+
+  	/* take time of execution */
+  	gettimeofday(&time,NULL);
+	return time.tv_sec*1000.0 + time.tv_usec/1000.0;
 }
 
 int
 main(int argc, char* argv[])
 {
+
+	// create events to measure time
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	// another way to measure time
+	double initime = time();
 
 	int *matrixA,*matrixB,*matrixResult;
 	
@@ -52,13 +79,15 @@ main(int argc, char* argv[])
 	cudaMemcpy(matrixA_d,matrixA,N * N * sizeof(int),cudaMemcpyHostToDevice);
 	cudaMemcpy(matrixB_d,matrixB,N * N * sizeof(int),cudaMemcpyHostToDevice);
 		 
+	cudaEventRecord(start);
+	addMatrix<<<numBlock,numThreadsPerBlock>>>(matrixA_d,matrixB_d,matrixResult_d,N*N/numBlock*numThreadsPerBlock);
 
-	int numThreads = 15;
-	addMatrix<<<1,numThreads>>>(matrixA_d,matrixB_d,matrixResult_d,N*N/numThreads);
-	
+
 	// cudaDeviceSynchronize waits for the kernel to finish, and returns
     // any errors encountered during the launch.
 	cudaDeviceSynchronize();
+	cudaEventRecord(stop);
+	double endtime = time();
 	
 	cudaMemcpy(matrixResult,matrixResult_d,N*N*sizeof(int),cudaMemcpyDeviceToHost);
 
@@ -68,6 +97,14 @@ main(int argc, char* argv[])
 	cudaFree(matrixB_d);
 	cudaFree(matrixResult_d);
 
-	
-	
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	//printf("Time elapsed in DEVICE: %f milliseconds / %g seconds\n",milliseconds, milliseconds/1000);
+	printf("Time elapsed in DEVICE (%d,%d) N = %d : %g milliseconds / %g seconds\n", numBlock,numThreadsPerBlock,N,
+	endtime - initime,(endtime - initime)/1000);
 }
+
+/* SOME TIME measureS
+	Time elapsed in DEVICE (1,2) N = 3500 : 962.264 milliseconds / 0.962264 seconds
+
+*/
---
+++ CUDA/exec/add_two_matrix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# create directory only if it doesn't exist
+mkdir -p files
+mkdir -p files/add_two_matrix
+FILE="../files/add_two_matrix/$1"
+if [ -f "$FILE" ]; then
+    rm -f $FILE
+fi
+N_v=500
+numBlock_v=1
+numThreadsPerBlock_v=1
+for i in `seq 0 50 3000`; do
+    sudo $(which nvcc) ../add_two_matrix.cu --run -D N=$N_v, -D numBlock=$numBlock_v, -D numThreadsPerBlock=$numThreadsPerBlock_v >> $FILE
+    numThreadsPerBlock_v=$((numThreadsPerBlock_v + 1))
+
+    if [ $i -eq "1000" ]; then
+        numThreadsPerBlock_v=1
+        numBlock_v=2
+    fi
+    if [ $i -eq "2000" ]; then
+        numThreadsPerBlock_v=1
+        numBlock_v=3
+    fi
+done
+
+# delete file created
+rm a.out
