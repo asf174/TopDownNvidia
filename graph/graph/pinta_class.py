a59d6cb45d5302ccaac5efa3eaf32bc1a6d5f995
Alvaro Saiz <alvaro.saizf@alumnos.unican.es>
2020-11-18 11:12:41 +0100

    Actualizacion metodo representacion grafico

--- graph/graph/pinta_class.py
+++ graph/graph/pinta_class.py
@@ -8,35 +8,37 @@ import matplotlib.pyplot as plt
 
 
 class Grafica:
-     # Arguments Features
-    MIN_NUMBER_ARGUMENTS = 6
-    MAX_NUMBER_ARGUMENTS = 8
-    NUMBER_ARGUMENT_WITH_SOME_LIMITS = 7
-    NUMBER_ARGUMENT_WITH_ALL_LIMITS = 8
-    HELP_OPTION = "help"
- 
+    
 
-    def _addFeatures():
+    def _addFeatures(self):
         # Add title and axis names
         plt.title(sys.argv[1])
         plt.xlabel(sys.argv[2])
         plt.ylabel(sys.argv[3])
         pass
 
-    def _addData():
+    def _addData(self):
         # colours for graphs
         colourScatter = ['blue', 'red', 'orange', 'grey']
         colourPlot = ['blue', 'red', 'orange', 'grey']
         
         # introduce fileNames in array
-        files = sys.argv[4]    
-        files = argument.replace('[','')
-        files = argument.replace(']','')
-        files = argument.split(',')
+        fileNames = sys.argv[4]    
+        fileNames = fileNames.replace('[','')
+        fileNames = fileNames.replace(']','')
+        files = fileNames.split(',')
+        
+        # legends
+        legendsArgument = sys.argv[5]    
+        legendsArgument = legendsArgument.replace('[','')
+        legendsArgument = legendsArgument.replace(']','')
+        legends = legendsArgument.split(',')
+        
 
         x_axis = []
         y_axis = []
         count = 0
+        indexLegends = 0
         colourNumber = 0
         for fileName in files:
             # read file
@@ -54,25 +56,36 @@ class Grafica:
                     else:
                         y_axis.append(word)
                         count = 0
+            plt.plot(x_axis,y_axis,color=colourPlot[colourNumber], label = legends[indexLegends], zorder=4)
+            plt.legend(legends[indexLegends])
             plt.scatter(x_axis,y_axis,color=colourScatter[colourNumber],zorder=1)
-            plt.plot(x_axis,y_axis,color=colourPlot[colourNumber],zorder=2)
             colourNumber = colourNumber + 1
+            indexLegends = indexLegends + 1
             x_axis = []
             y_axis = []
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.07),
+          fancybox=True, shadow=True, ncol=5)
         pass
 
     def _check_arguments(self):
+        
+        # Arguments Features
         MIN_NUMBER_ARGUMENTS = 6
+        MAX_NUMBER_ARGUMENTS = 8
+        HELP_OPTION = "help"
+ 
         if len(sys.argv) < MIN_NUMBER_ARGUMENTS or len(sys.argv) > MAX_NUMBER_ARGUMENTS:
-            print("Entro aki")
             if len(sys.argv) == 1:
                 print("Error with number arguments")
             elif sys.argv[1] == HELP_OPTION:
-                print("python3 " + sys.argv[0] + " <title> <titleX> <titleY> [<file[0]>,<file[n]>] <nameFig> [<xLimStart>,<xLimEnd>,<XStep>], [<yLimStart>,<yLimEnd>,<Ystep>]")
+                print("python3 " + sys.argv[0] + " <title> <titleX> <titleY> [<file[0]>,<file[n]>]" +
+                    ", [<legend<[0]>,<legend<[n]>] [<xLimStart>,<xLimEnd>,<XStep>], [<yLimStart>,<yLimEnd>,<Ystep>]")
             sys.exit()
         pass
 
-    def _check_limits():
+    def _check_limits(self):
+        NUMBER_ARGUMENT_WITH_SOME_LIMITS = 7
+        NUMBER_ARGUMENT_WITH_ALL_LIMITS = 8
         if len(sys.argv) == NUMBER_ARGUMENT_WITH_SOME_LIMITS:
             argument = sys.argv[6]
             type_axis = argument[0]
@@ -119,18 +132,18 @@ class Grafica:
 
 
     def main(self):
-        _check_arguments()
-        _addFeatures()
-        _addData()
+        self._check_arguments()
+        self._addFeatures()
+        self._addData()
         
-        _check_limits()
+        self._check_limits()
 
         # show
         plt.grid()
         plt.show()
 
-        # save
-        plt.savefig(sys.argv[5])
+    pass
 
 
+# execute 
 Grafica().main()
