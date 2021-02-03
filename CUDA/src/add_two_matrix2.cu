b15509f94d8e81122aea959d21d61f73e81e2317
lpm57 <lpm57@alumnos.unican.es>
2021-02-03 17:54:41 +0100

    Update

--- CUDA/src/add_two_matrix2.cu
+++ CUDA/src/add_two_matrix2.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
 
 #ifndef N 
 	#define N 3500
@@ -57,10 +58,9 @@ main(int argc, char* argv[])
 	int *matrixA,*matrixB,*matrixResult;
 	
 	// create matrix
-	
 	for (int i = 0; i < N; i++) {
-		matrixA = (int *) malloc(N * N* sizeof(int));
-		matrixB = (int *) malloc(N * N* sizeof(int));
+		matrixA = (int *) malloc(N * N * sizeof(int));
+		matrixB = (int *) malloc(N * N * sizeof(int));
 		matrixResult = (int *) malloc(N * N * sizeof(int));
 	}
 	for(int i = 0; i < N*N; i++) {
@@ -76,24 +76,24 @@ main(int argc, char* argv[])
 	cudaMalloc((void **) &matrixB_d, N * N * sizeof(int));
 	cudaMalloc((void **) &matrixResult_d, N * N * sizeof(int));
 
-	cudaMemcpy(matrixA_d,matrixA,N * N * sizeof(int),cudaMemcpyHostToDevice);
-	cudaMemcpy(matrixB_d,matrixB,N * N * sizeof(int),cudaMemcpyHostToDevice);
+	cudaMemcpy(matrixA_d, matrixA, N * N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(matrixB_d, matrixB, N * N * sizeof(int), cudaMemcpyHostToDevice);
 		 
 	cudaEventRecord(start);
-	printf("NUm bloques %d\n",(int) numBlock);
+	
+	cudaProfilerStart();
 	addMatrix<<<numBlock,numThreadsPerBlock>>>(matrixA_d,matrixB_d,matrixResult_d,N*N);
-
 
 	// cudaDeviceSynchronize waits for the kernel to finish, and returns
     // any errors encountered during the launch.
 	cudaDeviceSynchronize();
+	cudaProfilerStop();
 	cudaEventRecord(stop);
 	double endtime = time();
 	
 	cudaMemcpy(matrixResult,matrixResult_d,N*N*sizeof(int),cudaMemcpyDeviceToHost);
 
 	//printMtx(matrixResult);
-
 	//printMtx(matrixA);
 	//printMtx(matrixB);
 	//printMtx(matrixResult);
--- CUDA/src/hello_world.cu
+++ CUDA/src/hello_world.cu
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <cuda_profiler_api.h>
+
 
 __global__ void printHola() {
 	int idx = blockDim.x*blockIdx.x + threadIdx.x;
 	//printf("Hola mundo desde el device con thread: %d\n",idx);
+	// printf para evitar que salga warning al compilar
 }
 
 int main() {
 	//printf("Hola Mundo desde el host\n");
 
-	printHola<<<1,2>>>();
+	cudaProfilerStart();
+	printHola<<<1,65>>>();
 	cudaDeviceSynchronize();
+	cudaProfilerStop();
 	return 0;
 }
