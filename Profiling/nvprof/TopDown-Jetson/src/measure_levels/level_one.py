063937e75855f4148c16f01cd61ad71c196f9242
lpm57 <lpm57@alumnos.unican.es>
2021-03-17 22:34:25 +0100

    Update NSIGHT options

--- Profiling/nvprof/TopDown-Jetson/src/measure_levels/level_one.py
+++ Profiling/nvprof/TopDown-Jetson/src/measure_levels/level_one.py
@@ -46,7 +46,7 @@ class LevelOne(LevelExecution):
         Returns:
             String with command to be executed
         """
-
+        
         command : str = ("sudo $(which nvprof) --metrics " + self._front_end.metrics_str() + 
             "," + self._back_end.metrics_str() + "," + self._divergence.metrics_str() + "," + self._extra_measure.metrics_str()
             + "," + self._retire.metrics_str() + "  --events " + self._front_end.events_str() + 
