1094b4611fe0ba7847b238b1e54a582f149e462f
asf174-D <asf174@unican.es>
2021-07-07 15:27:19 +0200

    Update BUG LEVEL 2 NVPROF

--- src/measure_levels/level_two_nvprof.py
+++ src/measure_levels/level_two_nvprof.py
@@ -42,7 +42,7 @@ class LevelTwoNvprof(LevelTwo, LevelOneNvprof):
         self._back_memory_bound : BackMemoryBoundNvprof = back_memory_bound
         self._front_band_width : FrontBandWidthNvprof = front_band_width
         self._front_dependency : FrontDependencyNvprof = front_dependency
-         self._branch_divergence : DivergenceBranchNvprof = DivergenceBranchNvprof(DivergenceBranchParameters.C_DIVERGENCE_BRANCH_NAME, DivergenceBranchParameters.C_DIVERGENCE_BRANCH_DESCRIPTION, "", "")
+        self._branch_divergence : DivergenceBranchNvprof = DivergenceBranchNvprof(DivergenceBranchParameters.C_DIVERGENCE_BRANCH_NAME, DivergenceBranchParameters.C_DIVERGENCE_BRANCH_DESCRIPTION, "", "")
         self._replay_divergence : DivergenceReplayNvprof = DivergenceReplayNvprof(DivergenceReplayParameters.C_DIVERGENCE_REPLAY_NAME, DivergenceReplayParameters.C_DIVERGENCE_REPLAY_DESCRIPTION, "", "")
         super().__init__(program, input_file, output_file, output_scan_file, collect_metrics, collect_events, front_end, back_end, divergence, retire, extra_measure)
         pass
